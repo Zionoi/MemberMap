c8dea23f6a1ea596a69a264713bd2bf88525ab0b
Zionoi <163436710+Zionoi@users.noreply.github.com>
2025-03-09 20:28:40 +0900

    membermap 환경세팅

---
+++ MemberMap_BE/src/main/java/com/membermap/MemberMapBeApplication.java
@@ -0,0 +1,13 @@
+package com.membermap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberMapBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberMapBeApplication.class, args);
+	}
+
+}
---
+++ MemberMap_BE/src/test/java/com/membermap/MemberMapBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.membermap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberMapBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
